82e0810aacdcb1c20dd4b377799a5f67fc8f8874
Logan Nguyễn <lgad31vn@res-dhcp-129-3-137-91.oswego.edu>
2021-10-19 00:33:42 -0400

    first commit

---
+++ src/IntTreeNode.java
@@ -0,0 +1,34 @@
+/* Nam Phu Nguyen (Logan)
+   IT220-JAVA II 
+   Programing Assignment
+ */ 
+
+//create a TreeNode class -- similar to ListNode
+public class IntTreeNode {
+    //3 instance variables
+    public int data;
+    public IntTreeNode left;
+    public IntTreeNode right;
+
+    //3 constructors
+
+    //default constructor -- create a leaf
+    public IntTreeNode(){
+        this(0, null, null);
+    }
+
+    //one parameter constructor -- create a leaf
+    public IntTreeNode(int data){
+        this(data, null,null);
+    }
+
+    //3 parameters constructor
+    public IntTreeNode(int data, IntTreeNode left, IntTreeNode right){
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
+    //
+
+}
